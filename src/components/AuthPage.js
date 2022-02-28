82bb8600fbf29adcc010a23c95a5f34b91f54638
overgrowninfnat <giuseppedigiorno@yahoo.it>
2022-02-28 15:18:41 +0100

    starter template

--- src/components/AuthPage.js
+++ src/components/AuthPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function AuthPage() {
   return(
     <div className='w-screen h-screen flex justify-center items-center bg-gradient-to-b from-indigo-400 to-purple-700'>
-      <div className='max-w-sm h-auto p-20 bg-gradient-to-r from-indigo-600 to-purple-900 border-2 border-black rounded-xl shadow-lg'>
+      <div className='max-w-md h-auto p-20 bg-gradient-to-r from-indigo-600 to-purple-900 border-2 border-black rounded-xl shadow-lg'>
         <button className='bg-orange-500 text-white font-bold px-4 py-2 rounded-md shadow-md hover:bg-orange-600 hover:shadow-2xl transition duration-300'>
           Auth with Metamask
         </button>
